34f518d2c34cdeb5144a46e1b79d7b50d848dab9
Roman Myshko <romamyshko12@gmail.com>
2022-10-27 13:28:58 +0300

    Pr_2 done.

--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +14,13 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout3);
-        Button btn1 = (Button) findViewById(R.id.button16);
-        btn1.setOnClickListener(new View.OnClickListener() {
 
+        Button btn2 = (Button) findViewById(R.id.button16);
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                btn2.setBackgroundColor(Color.GREEN);
+
             }
         });
     }
